dca943b647b7da3ca93e1a811592c6f3a939cefb
Joshua Clayton <jclayton@thoughtbot.com>
2010-08-29 10:24:06 -0400

    Use threads

--- ext/sieve/sieve.c
+++ ext/sieve/sieve.c
@@ -1,5 +1,6 @@
 #include <ruby.h>
 #include <math.h>
+#include <pthread.h>
 #include "sieve.h"
 
 void Init_sieve() {
@@ -7,6 +8,14 @@ void Init_sieve() {
         numeric   = rb_define_class("Numeric", rb_cObject);
   rb_define_method(rb_mSieve, "sieve", sieve, 0);
   rb_include_module(numeric, rb_mSieve);
+}
+
+static void* clear_numbers(void* arg) {
+  param* params = (param *)arg;
+  long n;
+  for(n = params->start; n < params->end; n += params->step) {
+    params->numbers[n] = -1;
+  }
 }
 
 static VALUE sieve(const VALUE self) {
@@ -24,17 +33,46 @@ static VALUE sieve(const VALUE self) {
   }
   numbers[0] = numbers[1] = -1;
 
-  long current_square;
-  for(i = 0; i < number; i++) {
-    current_square = powl(i, 2);
-    if(numbers[i] == -1)        { continue; }
-    if(current_square > number) { break; }
+  long thread_count = 5;
 
-    long n;
-    for(n = current_square; n < number; n += i) {
-      numbers[n] = -1;
+  long current_square;
+  pthread_t* threads = malloc(thread_count * sizeof(pthread_t));
+  param* params = malloc(thread_count * sizeof(param));
+
+  for(i = 0; i < number; i += thread_count) {
+    int broken = 0;
+    long thread_incr, thread_incr_stop = -1;
+
+    for(thread_incr = 0; thread_incr < thread_count; thread_incr++) {
+      long current_i = thread_incr + i;
+      current_square = powl(current_i, 2);
+
+      if(numbers[current_i] == -1) { continue; }
+      if(current_square > number) {
+        broken = 1;
+        thread_incr_stop = thread_incr;
+        break;
+      }
+
+      params[thread_incr].numbers = numbers;
+      params[thread_incr].start   = current_square;
+      params[thread_incr].end     = number;
+      params[thread_incr].step    = current_i;
+
+      pthread_create(&threads[thread_incr],
+                     NULL,
+                     clear_numbers, (void *)(params + thread_incr));
     }
+
+    for(thread_incr = 0; thread_incr < (thread_incr_stop > -1 ? thread_incr_stop : thread_count); thread_incr++) {
+      pthread_join(threads[thread_incr], NULL);
+    }
+
+    if(broken == 1) { break; } // bubble break
   }
+
+  free(threads);
+  free(params);
 
   VALUE primes_array = rb_ary_new();
   for(i = 0; i < number; i++) {
--- ext/sieve/sieve.h
+++ ext/sieve/sieve.h
@@ -1,4 +1,12 @@
 #ifndef __sieve_h__
 #define __sieve_h__
 static VALUE sieve(VALUE);
+
+typedef struct {
+  long* numbers;
+  long start;
+  long end;
+  long step;
+} param;
+
 #endif
