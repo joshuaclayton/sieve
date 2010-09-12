09661e69d05aad92ee79b3a01c06b5197c37488c
Joshua Clayton <jclayton@thoughtbot.com>
2010-09-12 15:38:29 -0400

    Optimize array to store 1 or 0 for whether the number is prime, since there's a loop that already has the number itself

--- ext/sieve/sieve.c
+++ ext/sieve/sieve.c
@@ -11,34 +11,35 @@ void Init_sieve() {
 
 static VALUE sieve(const VALUE self) {
   if(NUM2LONG(self) < 2) { return Qnil; }
-  long number = NUM2LONG(self) + 1,
-     * numbers = malloc(number * sizeof(long));
+  long number = NUM2LONG(self) + 1;
+  short* numbers = malloc(number * sizeof(short));
 
   if(numbers == NULL) {
     rb_raise(rb_eNoMemError, "Can't allocate enough memory.");
   }
 
-  numbers[0] = numbers[1] = -1;
+  numbers[0] = numbers[1] = 0;
   long i;
-  for(i = 2; i < number; i++) { numbers[i] = i; }
+  for(i = 2; i < number; i++) { numbers[i] = 1; }
 
   long current_square;
   for(i = 0; i < number; i++) {
-    if(numbers[i] == -1) { continue; }
+    if(numbers[i] == 0) { continue; }
 
     current_square = powl(i, 2);
     if(current_square > number) { break; }
 
     long n;
     for(n = current_square; n < number; n += i) {
-      numbers[n] = -1;
+      numbers[n] = 0;
     }
   }
 
   VALUE primes_array = rb_ary_new();
   for(i = 0; i < number; i++) {
-    if(numbers[i] == -1) { continue; }
-    rb_ary_push(primes_array, LONG2FIX(numbers[i]));
+    if(numbers[i] == 1) {
+      rb_ary_push(primes_array, LONG2FIX(i));
+    }
   }
 
   free(numbers);
